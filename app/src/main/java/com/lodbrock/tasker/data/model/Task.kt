ab55aa604523a70b3a59026db3961423b61fb1d9
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-06 13:14:10 +0200

    added done variable

--- app/src/main/java/com/lodbrock/tasker/data/model/Task.kt
+++ app/src/main/java/com/lodbrock/tasker/data/model/Task.kt
@@ -6,6 +6,7 @@ class Task(
         var id : Long? = null,
         var title : String,
         var description : String ="",
+        var done : Boolean = false,
         var setToDate : Calendar,
         var modifiedAt : Calendar = Calendar.getInstance(),
         var createAt : Calendar = Calendar.getInstance()
