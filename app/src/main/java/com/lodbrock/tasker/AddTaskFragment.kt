c3777586870028384493e6219b9ce25e4c4f1677
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-10 21:29:51 +0200

    Made AddTaskFragment to use ViewModel

--- app/src/main/java/com/lodbrock/tasker/AddTaskFragment.kt
+++ app/src/main/java/com/lodbrock/tasker/AddTaskFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.lodbrock.tasker.data.model.Task
 import com.lodbrock.tasker.databinding.FragmentAddTaskBinding
 import com.lodbrock.tasker.util.YearDayMonth
+import com.lodbrock.tasker.viewmodels.AddTaskViewModel
+import com.lodbrock.tasker.viewmodels.HomeViewModel
+import java.util.*
 
 class AddTaskFragment : Fragment() {
 
     private lateinit var binding : FragmentAddTaskBinding
+
+    private val viewModel : AddTaskViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,9 +37,9 @@ class AddTaskFragment : Fragment() {
                 setToDate = YearDayMonth.today()
             )
 
-            val action = AddTaskFragmentDirections
-                .actionAddTaskFragmentToHomeFragment(taskToAddOnStart = task)
-                findNavController().navigate(action)
+            viewModel.addTask(task)
+            Toast.makeText(context, "Task was added", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addTaskFragment_to_homeFragment)
         }
 
         return binding.root
