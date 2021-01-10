51fa467d1746ac70ce00ed6b7b37ebb207151848
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-10 20:20:16 +0200

    Made add task to send task to home page

--- app/src/main/java/com/lodbrock/tasker/AddTaskFragment.kt
+++ app/src/main/java/com/lodbrock/tasker/AddTaskFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.lodbrock.tasker.data.model.Task
 import com.lodbrock.tasker.databinding.FragmentAddTaskBinding
+import com.lodbrock.tasker.util.YearDayMonth
 
 class AddTaskFragment : Fragment() {
 
@@ -19,6 +22,18 @@ class AddTaskFragment : Fragment() {
         binding = FragmentAddTaskBinding.inflate(inflater, container, false)
 
         configSwitch()
+
+        binding.addTaskBtn.setOnClickListener{
+            val task = Task(
+                title = binding.titleEdit.text.toString(),
+                description = binding.descriptionEdit.text.toString(),
+                setToDate = YearDayMonth.today()
+            )
+
+            val action = AddTaskFragmentDirections
+                .actionAddTaskFragmentToHomeFragment(taskToAddOnStart = task)
+                findNavController().navigate(action)
+        }
 
         return binding.root
     }
