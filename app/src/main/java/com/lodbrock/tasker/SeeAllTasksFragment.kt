2cd7939f69de226338d4bdec119c727a41934268
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-12 17:22:14 +0200

    Made fragment to work

---
+++ app/src/main/java/com/lodbrock/tasker/SeeAllTasksFragment.kt
@@ -0,0 +1,70 @@
+package com.lodbrock.tasker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.applandeo.materialcalendarview.EventDay
+import com.lodbrock.tasker.databinding.FragmentSeeAllTasksBinding
+import com.lodbrock.tasker.ui.adapters.TaskArchiveRecyclerAdapter
+import com.lodbrock.tasker.util.YearDayMonth
+import com.lodbrock.tasker.viewmodels.SeeAllTasksViewModel
+
+class SeeAllTasksFragment : Fragment() {
+
+    private lateinit var binding : FragmentSeeAllTasksBinding
+
+    private val taskArchiveAdapter = TaskArchiveRecyclerAdapter(listOf())
+
+    private val viewModel : SeeAllTasksViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentSeeAllTasksBinding.inflate(layoutInflater, container, false)
+
+        viewModel.allTasksForToday.observe(viewLifecycleOwner, {
+            taskArchiveAdapter.setTaskList(
+                viewModel.getTaskForDate(YearDayMonth.today())
+            )
+            if (taskArchiveAdapter.itemCount == 0) showEmptyListHint() else hideEmptyListHint()
+        })
+
+        viewModel.allEventDates.observe(viewLifecycleOwner, { dates ->
+            val events: MutableList<EventDay> = ArrayList()
+
+            dates.forEach { date ->
+                events.add(EventDay(date, R.drawable.ic_circle, R.color.red_200))
+            }
+
+            binding.applandeoCalendar.setEvents(events)
+        })
+
+        binding.taskArchiveRecycler.adapter = taskArchiveAdapter
+
+        binding.applandeoCalendar.setOnDayClickListener {
+            val date = YearDayMonth.fromCalendar(it.calendar)
+            taskArchiveAdapter.setTaskList(viewModel.getTaskForDate(date))
+            val status = "All tasks for ${date.month+1}.${date.day}"
+            binding.archiveStatusLabel.text = status
+            println(taskArchiveAdapter.itemCount == 0)
+            if (taskArchiveAdapter.itemCount == 0) showEmptyListHint() else hideEmptyListHint()
+        }
+
+        return binding.root
+    }
+
+    private fun hideEmptyListHint() {
+        binding.archiveEmptyListLabel.visibility = GONE
+    }
+
+    private fun showEmptyListHint() {
+        binding.archiveEmptyListLabel.visibility = VISIBLE
+    }
+
+}
