b221571517e6e11b2ab45f4cf6c483947f571fc1
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-12 17:17:28 +0200

    Added compare method and toCalendar

--- app/src/main/java/com/lodbrock/tasker/util/YearDayMonth.kt
+++ app/src/main/java/com/lodbrock/tasker/util/YearDayMonth.kt
@@ -9,6 +9,13 @@ class YearDayMonth(
     val month: Int,
     val day: Int
 ):Parcelable {
+
+    fun toCalendar() : Calendar {
+        val c = Calendar.getInstance()
+        c.set(year, month, day)
+        return c
+    }
+
     override fun toString(): String {
         return "$year:$month:$day"
     }
@@ -40,6 +47,27 @@ class YearDayMonth(
                 now.get(Calendar.YEAR),
                 now.get(Calendar.MONTH),
                 now.get(Calendar.DAY_OF_MONTH)
+            )
+        }
+
+        fun compare(ydm1: YearDayMonth, ydm2: YearDayMonth): Int {
+            if(ydm2.year > ydm1.year) return -1
+            else if (ydm2.year < ydm1.year) return 1
+
+            if(ydm2.month > ydm1.month) return -1
+            else if (ydm2.month < ydm1.month) return 1
+
+            if(ydm2.day > ydm1.day) return -1
+            else if (ydm2.day < ydm1.day) return 1
+
+            return 0
+        }
+
+        fun fromCalendar(calendar: Calendar) : YearDayMonth {
+            return YearDayMonth(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
             )
         }
     }
