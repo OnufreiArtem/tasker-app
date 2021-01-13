d167ab1046bd22f5af9f1d4e6c433e931e941e6e
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-14 00:06:01 +0200

    Added repository

---
+++ app/src/main/java/com/lodbrock/tasker/data/repositories/AppRepository.kt
@@ -0,0 +1,63 @@
+package com.lodbrock.tasker.data.repositories
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.lodbrock.tasker.data.database.AppDatabase
+import com.lodbrock.tasker.data.model.Task
+import com.lodbrock.tasker.util.YearDayMonth
+import java.util.*
+
+class AppRepository(context: Context) {
+
+    private val tag = "APP_REPOSITORY"
+
+    private val database = AppDatabase.getDatabase(context)
+    private val taskDao = database.taskDao()
+
+    fun allTasks() : LiveData<List<Task>> {
+        return taskDao?.allTasks() ?: MutableLiveData(listOf())
+    }
+
+    fun getTasksForDate(date: YearDayMonth) : LiveData<List<Task>> {
+        return taskDao?.tasksForDate(date) ?: MutableLiveData(listOf())
+    }
+
+    suspend fun addTasks(task: Task){
+        taskDao?.let {
+            it.addTask(task)
+            Log.d(tag, "Added $task")
+        } ?: Log.d(tag, "Unable add $task")
+    }
+
+    suspend fun makeTaskDone(task: Task){
+        taskDao?.let {
+            it.updateTask( task.apply {
+                done = true
+                modifiedAt = Calendar.getInstance()
+            } )
+            Log.d(tag, "Task $task was updated and made done")
+        } ?: Log.d(tag, "Unable to update (make not done) $task")
+    }
+
+    suspend fun makeTaskNotDone(task: Task){
+        taskDao?.let {
+            if(it.updateTask( task.apply {
+                done = false
+                modifiedAt = Calendar.getInstance()
+            } ) > 0)
+                Log.d(tag, "Task $task was updated and made not done")
+        } ?: Log.d(tag, "Unable to update (make done) $task")
+    }
+
+    suspend fun deleteTask(task: Task) {
+        taskDao?.let {
+            if( it.deleteTask(task) > 0 ) Log.d(tag, "Task $task was deleted")
+        } ?: Log.d(tag, "Unable to delete $task")
+    }
+
+    fun allDatesWithTasks() : LiveData<List<YearDayMonth>> {
+        return taskDao?.allDatesWithTasks() ?: MutableLiveData(listOf())
+    }
+}
