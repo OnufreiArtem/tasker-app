ee2c2938ef1a9b1661db02a74fee593f4eb525d2
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-14 00:06:19 +0200

    Removed run in single thread

--- app/src/main/java/com/lodbrock/tasker/data/database/AppDatabase.kt
+++ app/src/main/java/com/lodbrock/tasker/data/database/AppDatabase.kt
@@ -29,7 +29,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).allowMainThreadQueries().build()
+                ).build()
                 INSTANCE = instance
                 return instance
             }
