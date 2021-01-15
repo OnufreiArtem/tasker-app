d6e107455f0310ba6f4f4d953ebb1c9008e0ae84
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-15 13:29:04 +0200

    Added item gestures listener for recycler view adapter

--- app/src/main/java/com/lodbrock/tasker/SeeAllTasksFragment.kt
+++ app/src/main/java/com/lodbrock/tasker/SeeAllTasksFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.applandeo.materialcalendarview.EventDay
+import com.google.android.material.snackbar.Snackbar
 import com.lodbrock.tasker.data.model.Task
 import com.lodbrock.tasker.databinding.FragmentSeeAllTasksBinding
 import com.lodbrock.tasker.ui.adapters.ItemClickListener
 import com.lodbrock.tasker.ui.adapters.TaskArchiveRecyclerAdapter
+import com.lodbrock.tasker.util.TextUtil
 import com.lodbrock.tasker.util.YearDayMonth
 import com.lodbrock.tasker.viewmodels.SeeAllTasksViewModel
 import java.text.DateFormat
@@ -47,6 +53,7 @@ class SeeAllTasksFragment : Fragment() {
         registerObservables()
 
         binding.taskArchiveRecycler.adapter = taskArchiveAdapter
+        ItemTouchHelper(itemTouchCallback).attachToRecyclerView(binding.taskArchiveRecycler)
 
         binding.applandeoCalendar.setOnDayClickListener {
             val date = YearDayMonth.fromCalendar(it.calendar)
@@ -85,4 +92,47 @@ class SeeAllTasksFragment : Fragment() {
         binding.archiveEmptyListLabel.visibility = VISIBLE
     }
 
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder,
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val task = taskArchiveAdapter.getTaskAtPosition(viewHolder.adapterPosition)
+            task?.let {
+                val result = viewModel.deleteTask(task)
+
+                if (result) {
+                    val textColor = ContextCompat.getColor(viewHolder.itemView.context,
+                        R.color.white)
+                    val bcgColor = ContextCompat.getColor(viewHolder.itemView.context,
+                        R.color.dark_blue)
+                    val actionColor = ContextCompat.getColor(viewHolder.itemView.context,
+                        R.color.red_200)
+
+                    Snackbar.make(viewHolder.itemView,
+                        "\"${TextUtil.threeDotLine(task.title, 15)}\" was deleted",
+                        Snackbar.LENGTH_LONG)
+                        .setTextColor(textColor)
+                        .setBackgroundTint(bcgColor)
+                        .setActionTextColor(actionColor)
+                        .setAction("Undo") {
+                            viewModel.addTask(task)
+                        }
+                        .show()
+                } else {
+                    Toast.makeText(context, "Failed to delete task", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+        }
+    }
 }
+
+
