045ca994af4532e09ba18adcbee7c4322a7b9d94
Artem Onufrei <2hilderoy2@gmail.com>
2021-01-17 11:41:45 +0200

    Added withDone to check dialog window style

--- app/src/main/java/com/lodbrock/tasker/util/TaskDialog.kt
+++ app/src/main/java/com/lodbrock/tasker/util/TaskDialog.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.SwitchCompat
 import com.lodbrock.tasker.R
 import com.lodbrock.tasker.data.model.Task
 
@@ -15,11 +16,15 @@ class TaskDialog(private val activity: Activity?, private val inflater: LayoutIn
         dialogTitle: String,
         dialogPositiveBtnText: String,
         positiveClickListener: OnDialogClickListener?,
-        negativeClickListener: OnDialogClickListener?
+        negativeClickListener: OnDialogClickListener?,
+        withDone: Boolean = false
     ) {
         activity?.let {
             val header: View = inflater.inflate(R.layout.dialog_title, null)
-            val content: View = inflater.inflate(R.layout.dialog_add_task, null)
+            val content: View = inflater.inflate(
+                if(!withDone) R.layout.dialog_add_task else R.layout.dialog_add_task_with_done,
+                null
+            )
 
             val headerTitle = header.findViewById<TextView>(R.id.dialog_title)
             headerTitle.text = dialogTitle
@@ -34,11 +39,16 @@ class TaskDialog(private val activity: Activity?, private val inflater: LayoutIn
                         val title = content.findViewById<EditText>(R.id.dialog_title_edit)
                         val description =
                             content.findViewById<EditText>(R.id.dialog_description_edit)
+                        val done = if(!withDone) false else {
+                            val switch = content.findViewById<SwitchCompat>(R.id.switch_is_task_done)
+                            switch.isChecked
+                        }
 
                         positiveClickListener?.let { positiveClickListener.onClick(Task(
                             title = title.text.toString(),
                             description = description.text.toString(),
-                            setToDate = YearDayMonth.today()
+                            setToDate = YearDayMonth.today(),
+                            done = done
                         )) }
 
                         dialog.dismiss()
